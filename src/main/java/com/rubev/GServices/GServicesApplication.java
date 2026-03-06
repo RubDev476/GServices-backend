8812d36c42ba317fbec4275f17b3d195e821a8db
ruben-palma <ruben.palma@dineroinmediato.com.mx>
2026-03-06 17:42:21 -0600

    Initial commit

---
+++ src/main/java/com/rubev/GServices/GServicesApplication.java
@@ -0,0 +1,13 @@
+package com.rubev.GServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubev/GServices/GServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubev.GServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
